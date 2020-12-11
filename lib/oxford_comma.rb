6eeb53d1332a71f6e9800cce374c098f28a4d34b
Gerald <geraldarzola@gmail.com>
2020-12-11 15:07:36 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
+    arrsize = array.size
+    if arrsize == 1
+        x = array.join
+        return x
+    elsif arrsize == 2
+        x = array.join(" and ")
+        return x
+    else
+        last = array.pop
+        first = array.join(", ")
+        return x = first + ", and " + last
+
+    end
 
 end
